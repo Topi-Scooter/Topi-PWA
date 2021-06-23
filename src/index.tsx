12d97ccae3de55a5bfd50f42488c8694bbd49a23
Allen Harper <aharper@prostarcorp.com>
2021-06-22 21:18:09 -0600

    Removed BottomMenu_old.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,11 @@ import './index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+
+/*
+Allen - Had to remove <React.StrictMode> tag
+https://github.com/mui-org/material-ui/issues/13394#issuecomment-433357852
+*/
