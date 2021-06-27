55409e545a6e5bbbaa69c48d4102c0efbe854ae8
Allen Harper <aharper@prostarcorp.com>
2021-06-26 20:47:37 -0600

    Added/configured AWS Amplify auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import './App.scss';
 import { MapPage } from './pages/MapPage'
+
 
 function App() {
   return (
     <div className="App">
       <MapPage/>
+      {/* TODO move sign out button*/}
+      <AmplifySignOut /> 
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
 
