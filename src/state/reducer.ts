f56eb78eae4925c3f250e1156f62c68c52abb93d
Allen Harper <aharper@prostarcorp.com>
2021-07-19 01:20:05 -0600

    Created AppState with React.useContext & React.useReducer

---
+++ src/state/reducer.ts
@@ -0,0 +1,22 @@
+// https://codesandbox.io/s/quirky-grass-4f0yf?fontsize=14&hidenavigation=1&theme=dark&file=/src/state/reducer.ts
+// https://www.youtube.com/watch?v=kK_Wqx3RnHk
+import { AppState } from './state'
+import { ActionType, SetIsRiding, StateActions } from './actions'
+
+export function AppReducer(state: AppState, action: StateActions): AppState {
+    switch (action.type) {
+        case ActionType.SetIsRiding:
+            let newState = {
+                ...state,
+                isRiding: action.payload
+            }
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export const setIsRiding = (isRiding: boolean): SetIsRiding => ({
+    type: ActionType.SetIsRiding,
+    payload: isRiding,
+})
