9e50db4b90803c72f0a1e314379d8b356f82c1f7
Dylan Taylor <dtaylor6306@gmail.com>
2021-11-18 13:54:11 -0700

    more progress on resetting pw

--- frontend/src/pages/ChangePasswordPage.tsx
+++ frontend/src/pages/ChangePasswordPage.tsx
@@ -1,17 +1,49 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement,useState} from 'react'
 import { Link } from 'react-router-dom'
 import { TopMenu } from '../components/TopMenu'
+import { Auth } from 'aws-amplify';
+import { AppContext } from '../state/context';
 interface Props {
     
 }
 
 function ChangePasswordPage(props: Props): ReactElement {
 
+    const [password,setPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+
+    function handleSubmit(e:any){
+        e.preventDefault()
+        console.log("submitted")
+
+    }
+
+    Auth.currentAuthenticatedUser()
+    .then(user => {
+        return Auth.changePassword(user, 'oldPassword', 'newPassword');
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+    //onChange = {(event) => setPassword(event.target.value)}
+    //onChange = {(event) => setNewPassword(event.target.value)}
 
     return (
         <div>
             <TopMenu/>
-            This is where you will change the password
+            <form onSubmit = {handleSubmit}>
+                <label> Current Password: </label>
+                    <input 
+                        value = {password} 
+                        onChange = {(event) => setPassword(event.target.value)}
+                />
+
+                <label> New Password: </label>
+                    <input 
+                        value = {newPassword} 
+                        onChange = {(event) => setNewPassword(event.target.value)}
+                />
+            <button type ="submit" >Submit</button>
+            </form>
   
         </div>
 
