332efad873dbcd340567cd4162d44da26849fc38
Dylan Taylor <dtaylor6306@gmail.com>
2021-11-23 21:59:14 -0700

    additions to changepassword

--- frontend/src/pages/ChangePasswordPage.tsx
+++ frontend/src/pages/ChangePasswordPage.tsx
@@ -1,8 +1,7 @@
-import React, { ReactElement,useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { ReactElement,useState, useEffect} from 'react'
 import { TopMenu } from '../components/TopMenu'
 import { Auth } from 'aws-amplify';
-import { AppContext } from '../state/context';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 interface Props {
     
 }
@@ -15,30 +14,29 @@ function ChangePasswordPage(props: Props): ReactElement {
     function handleSubmit(e:any){
         e.preventDefault()
         console.log("submitted")
-
+        
+        Auth.currentAuthenticatedUser()
+        .then(user => {
+            return Auth.changePassword(user, password,newPassword );
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
     }
 
-    Auth.currentAuthenticatedUser()
-    .then(user => {
-        return Auth.changePassword(user, 'oldPassword', 'newPassword');
-    })
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-    //onChange = {(event) => setPassword(event.target.value)}
-    //onChange = {(event) => setNewPassword(event.target.value)}
+
 
     return (
-        <div>
+        <div className = "changePassword">
             <TopMenu/>
             <form onSubmit = {handleSubmit}>
                 <label> Current Password: </label>
-                    <input 
+                    <input type = 'password'
                         value = {password} 
                         onChange = {(event) => setPassword(event.target.value)}
                 />
 
                 <label> New Password: </label>
-                    <input 
+                    <input type = 'password' 
                         value = {newPassword} 
                         onChange = {(event) => setNewPassword(event.target.value)}
                 />
@@ -49,5 +47,4 @@ function ChangePasswordPage(props: Props): ReactElement {
 
     )
 }
-
-export default ChangePasswordPage
+export default withAuthenticator(ChangePasswordPage);
