5fd1a030360078828ea08fe6fa632bbae7b2c304
Dylan Taylor <dtaylor6306@gmail.com>
2021-11-26 22:01:19 -0700

    Updated TextFields for Change PAssword

--- frontend/src/pages/ChangePasswordPage.tsx
+++ frontend/src/pages/ChangePasswordPage.tsx
@@ -3,11 +3,16 @@ import { TopMenu } from '../components/TopMenu'
 import { Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+
 interface Props {
     
 }
 
-
+//TODO Handle error or success for change password
 function ChangePasswordPage(props: Props): ReactElement {
 
     const [password,setPassword] = useState("");
@@ -31,23 +36,38 @@ function ChangePasswordPage(props: Props): ReactElement {
         <div className = "changePassword">
             <TopMenu/>
             <div className = "changePassword-Form">
-
-                <form onSubmit = {handleSubmit}>
-                    <label> Current Password: </label>
-                        <input type = 'password'
-                            value = {password} 
-                            onChange = {(event) => setPassword(event.target.value)}
-                            />
-
-                    <label> New Password: </label>
-                        <input type = 'password' 
-                            value = {newPassword} 
-                            onChange = {(event) => setNewPassword(event.target.value)}
-                            />
+            <Box      
+                component="form"
+                sx={{
+        '       & .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                onSubmit = {handleSubmit}
+                noValidate
+                autoComplete="off"
+            >   <div>
+                    <TextField
+                    required
+                    id="outlined-required"
+                    label="Current Password"
+                    value = {password} 
+                    onChange = {(event) => setPassword(event.target.value)}
+                    type ="password"
+                    />
+                    <TextField
+                    required
+                    id="outlined-required"
+                    label="New Password"
+                    value = {newPassword} 
+                    onChange = {(event) => setNewPassword(event.target.value)}
+                    type ="password"
+                    />
+                </div>
                 <Button type ="submit" variant= "contained">Submit</Button>
-                </form>
-            
+                
+            </Box>
             </div>
+            
+            
   
         </div>
 
