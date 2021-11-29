a47f80df25da913b6d1718479c0287546553585f
Dylan Taylor <dtaylor6306@gmail.com>
2021-11-29 13:49:31 -0700

    updating profile

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,15 +1,26 @@
-import React, { ReactElement } from 'react'
-import { TopMenu } from '../components/TopMenu'
+import React, { ReactElement,useState } from 'react';
+import { TopMenu } from '../components/TopMenu';
+import { Auth } from 'aws-amplify';
 
 interface Props {
     
 }
 
+
 export default function ProfilePage(props: Props): ReactElement {
+    const [email,setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    Auth.currentAuthenticatedUser().then((user)=> setPhoneNumber(user.attributes.phone_number));
+    Auth.currentAuthenticatedUser().then((user)=> setEmail(user.attributes.email));
+    const textAlign={
+        textAlign: 'center' as 'center'
+    }
     return (
-        <div>
+        <div style= {textAlign} >
             <TopMenu/>
-            Profile Page
+            <h1>Profile Page</h1>
+            <p> {email}</p>
+            <p>{phoneNumber}</p>
         </div>
     )
 }
