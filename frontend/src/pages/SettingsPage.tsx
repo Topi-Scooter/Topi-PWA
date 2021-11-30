68f2a3c606d765111a8389319bb6da8ce8001abd
Dylan Taylor <dtaylor6306@gmail.com>
2021-11-30 10:28:34 -0700

    Finished Styling for settings page

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { TopMenu } from '../components/TopMenu'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import { Auth } from 'aws-amplify';
 
 interface Props {
@@ -23,65 +25,100 @@ function SettingsPage(props: Props): ReactElement {
     return (
         <div>
             <TopMenu/>
-            Settings Page
-            <div>
-                <Box
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        p: 1,
-                        mt: 10,
-                        mx: 5
-        
-                    }}>
-                    <div style= {textAlign}>
-                    <h1>Change Password</h1>
-                    <p> A strong password is at least 8 characters long. </p>
-                    </div>
-                </Box>
-                <Box
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        p: 1,
-                        m: 5
-                    }}>
-                    <Button
-                        variant="outlined" 
-                        href="/changepassword"
-                    >Change Password</Button>
-                </Box>
-                <br></br>
-                <Box
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        p: 1,
-                        mt: 10,
-                        mx: 5
-                    }}>
-                    <div style= {textAlign}>
-                    <h1>Change Email</h1>
-                    <p> {email} </p>
-                    </div>
-                </Box>
-                <Box
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        p: 1,
-                        m: 5,
-                    }}>
-                    <Button
-                        variant="outlined" 
-                        href=""
-                    >Change Email</Button>
-                </Box>
-            </div>
-  
+        <Box
+        sx={{
+            p: 1,
+            my: 20,
+            mx: 5
+        }}>
+        <Paper>
+            <Grid item xs = {12}>
+            <h1>Change Password</h1>
+            <p> A strong password is at least 8 characters long.</p>
+            <Button
+                variant="outlined" 
+                href="/changepassword"
+               >Change</Button>
+            </Grid>
+        </Paper>
+        <br></br>
+        <Paper>
+                <Grid item xs = {12}>
+                <h1>Change Email</h1>
+                <p> {email} </p>
+                </Grid>
+                <Grid>
+                <Button
+                    variant="outlined" 
+                    href=""
+                   >Change</Button>
+                </Grid>
+        </Paper>
+        </Box>
         </div>
-
     )
+    // return (
+    //     <div>
+    //         <TopMenu/>
+    //         Settings Page
+    //         <div>
+    //             <Box
+    //                 component="div"
+    //                 sx={{
+    //                     display: 'inline-flex',
+    //                     p: 1,
+    //                     mt: 10,
+    //                     mx: 5
+        
+    //                 }}>
+    //                 <div style= {textAlign}>
+    //                 <h1>Change Password</h1>
+    //                 <p> A strong password is at least 8 characters long. </p>
+    //                 </div>
+    //             </Box>
+    //             <Box
+    //                 component="div"
+    //                 sx={{
+    //                     display: 'inline-flex',
+    //                     p: 1,
+    //                     m: 5
+    //                 }}>
+    //                 <Button
+    //                     variant="outlined" 
+    //                     href="/changepassword"
+    //                 >Change Password</Button>
+    //             </Box>
+    //             <br></br>
+    //             <Box
+    //                 component="div"
+    //                 sx={{
+    //                     display: 'inline-flex',
+    //                     p: 1,
+    //                     mt: 10,
+    //                     mx: 5
+    //                 }}>
+    //                 <div style= {textAlign}>
+    //                 <h1>Change Email</h1>
+    //                 <p> {email} </p>
+    //                 </div>
+    //             </Box>
+    //             <Box
+    //                 component="div"
+    //                 sx={{
+    //                     display: 'inline-flex',
+    //                     p: 1,
+    //                     m: 5,
+    //                 }}>
+    //                 <Button
+    //                     variant="outlined" 
+    //                     href=""
+    //                 >Change Email</Button>
+    //             </Box>
+    //         </div>
+  
+    //     </div>
+
+    // )
 }
 
 export default SettingsPage
