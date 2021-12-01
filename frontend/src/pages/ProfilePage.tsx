0a4e1669247eecfaeb5e7d8a01bdad5d4bf6b316
Dylan Taylor <dtaylor6306@gmail.com>
2021-11-30 21:47:17 -0700

    Added avatar img to profile

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -2,7 +2,8 @@ import React, { ReactElement,useState } from 'react';
 import { TopMenu } from '../components/TopMenu';
 import { Auth } from 'aws-amplify';
 import Avatar from '@mui/material/Avatar';
-
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 interface Props {
     
 }
@@ -23,13 +24,53 @@ export default function ProfilePage(props: Props): ReactElement {
     // sx={{ width: 128, height: 128 }}
     // />
     return (
-        <div className = "profile-div" >
+        <div>
             <TopMenu/>
+            <Paper
+            sx ={{
+                m:20,
+                p:5,
 
+            }}
+            >
+            <Box
+            sx ={{
+                m:1,
+                p:1,
+                alignItems: "center",
+                justifyContent: "center",
+                display: "flex"
+            
+            }}>
+            
+            <Avatar
+            alt= "B"
+            src= "/logo192.png"
+            sx={{ 
+                width: 100, 
+                height: 100, 
+                alignItems: "center",
+                justifyContent: "center",
+                
 
+            }}
+            />
+            </Box>
+            <br></br>
+            <Box
+            sx ={{
+                m:1,
+                p:1,
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center"       
+            }}>
+    
             <h1>Profile Page</h1>
             <p> {email}</p>
             <p>{phoneNumber}</p>
+            </Box>
+            </Paper>
         </div>
     )
 }
