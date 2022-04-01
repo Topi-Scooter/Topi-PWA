3b0e05450b0df5b485bba1dba822b2d96728ac74
Allen Harper <allen.harper9917@gmail.com>
2022-03-31 20:46:22 -0600

    get all scoots

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement, useContext, useState } from 'react'
-import {createScooter} from '../api/Scooter'
+import { IconButton, List, ListItem, ListItemText } from '@material-ui/core';
+import React, { ReactElement, useContext, useEffect, useState } from 'react'
+import {createScooter, getAllScooter} from '../api/Scooter'
 import { TopMenu } from '../components/TopMenu'
 import { AppContext } from '../state/context';
 
@@ -9,6 +10,7 @@ interface Props {
 
 function AdminPage(_props: Props): ReactElement {
     const [scoot, setscoot] = useState("scooter_00")
+    const [scooters, setScooters] = useState<any>()
     const { state, dispatch } = useContext(AppContext);
 
 
@@ -20,30 +22,55 @@ function AdminPage(_props: Props): ReactElement {
     function handleSubmit(e:any) {
         e.preventDefault()
         console.log(e)
-        const scooter = createScooter(scoot);
-        console.log(scooter)
+        createScooter(scoot).then((res) => {
+            console.log(res)
+            getScoots()
+        });
     }
+
+    function getScoots() {
+        getAllScooter()
+            .then((scoots:any)=>{
+                setScooters(scoots.data.listScooters.items);
+                console.log(scoots.data.listScooters.items)
+            })
+    }
+
+    useEffect(() => {
+        getScoots();
+    }, [])
+    
 
     return (
         <div>
-            <TopMenu/>
             Admin Page
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Scooter ID:
-                    <input type="text" name="name" onChange={handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <TopMenu/>
+            <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Scooter ID:
+                        <input type="text" name="name" onChange={handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+
+            <div>
+                <List>
+                    {scooters?.map((data:any) => (
+                        <ListItemText primary={`Line item ${data.id}`} />
+                    ))}
+                </List>
+            </div>
         </div>
     )
 }
